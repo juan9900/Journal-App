6adf0ca461079950c3e72e069c23b4081c1449da
juan9900 <juanluislauretta@gmail.com>
2023-07-26 20:20:27 -0400

    commit

--- src/journal/pages/HomePage.jsx
+++ src/journal/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
-
+import MailOutline from "@mui/icons-material/MailOutline";
 export const HomePage = () => {
   const navigate = useNavigate();
   const btnNavigateLogin = () => {
@@ -13,6 +13,7 @@ export const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
+      <MailOutline />
       <button onClick={btnNavigateLogin}> Go to Login Page</button>
       <button onClick={btnNavigateRegister}> Go to Register Page</button>
       <Button variant="contained">Hello World</Button>
